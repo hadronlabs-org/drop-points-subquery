4ea17f3e160f5032e173a5d18d515c003b39028f
Sergey Ratiashvili <serg.s.r@gmail.com>
2025-05-22 12:06:41 +0200

    feat: added mirror track

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,6 +1,5 @@
-import { CosmosEvent, CosmosMessage, CosmosTransaction } from "@subql/types-cosmos";
+import { CosmosEvent } from "@subql/types-cosmos";
 import { UserBond, UserBondPlain } from "../types";
-import { parse } from "path";
 
 
 export async function handleBondExecution(
@@ -19,6 +18,49 @@ export async function handleBondExecution(
       received: BigInt((received || '0').toString()),
       height: BigInt(input.tx.block.header.height),
       exchange_rate: parseFloat((exchange_rate || '0').toString()),
+    }
+  ).save();
+  if (!receiver) {
+    logger.error("receiver not found in attributes");
+    return;
+  }
+  if (!ref) {
+    logger.warn("reg not found in attributes");
+    return;
+  }
+  if (receiver === ref) {
+    logger.warn("User cannot refer himself");
+    return;
+  }
+  const prevUserBond = await UserBond.get(receiver.toString());
+  if (prevUserBond && receiver) {
+    logger.warn('User %s already bonded');
+    return;
+  }
+  await (UserBond.create({
+    id: receiver.toString(),
+    ref: ref.toString(),
+    height: BigInt(input.tx.block.header.height),
+    ts: BigInt(input.tx.block.header.time.getTime())
+  })).save();
+  logger.info("!!!!!! Bonded %s to %s", receiver, ref);
+}
+
+export async function handleMirrorBondExecution(
+  input: CosmosEvent
+): Promise<void> {
+  const { attributes } = input.event;
+  const receiver = attributes.find((attr) => attr.key === "receiver")?.value;
+  const ref = attributes.find((attr) => attr.key === "ref")?.value;
+  const received = attributes.find((attr) => attr.key === "coin")?.value.toString().replace(/ibc.*/gi,'');
+  await UserBondPlain.create(
+    {
+      id: input.tx.hash,
+      address: (receiver || '').toString(),
+      ref: (ref || '').toString(),
+      received: BigInt((received || '0').toString()),
+      height: BigInt(input.tx.block.header.height),
+      exchange_rate: 0,
     }
   ).save();
   if (!receiver) {
